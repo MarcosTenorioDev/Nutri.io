5a4d64892976c2cd30b16d7102bb8c11a8b7fcd4
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2023-06-22 13:39:45 -0300

    :sparkles: Feat: add function to connect with OpenAiApi

--- src/components/GenerateDiet.jsx
+++ src/components/GenerateDiet.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from 'react';
-import '../assets/css/GenerateDiet.css'
+import '../assets/css/GenerateDiet.css';
+import axios from "axios";
+import API_KEY from '../services/apikey';
+
 
 
 
@@ -18,6 +21,39 @@ const sendQuestion = (event) => {
     }   
 };
 
+
+function getChat(pergunta) {
+    const OPENAI_API_KEY =  API_KEY;
+    const content = pergunta;
+
+    const config = {
+      headers: {
+        Authorization: "Bearer " + OPENAI_API_KEY,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    };
+  
+    const data = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "Você é um assistente de bate-papo." },
+        { role: "user", content: content },
+      ],
+    };
+  
+    axios
+      .post("https://api.openai.com/v1/chat/completions", data, config)
+      .then((resposta) => {
+        console.log(resposta.data.choices[0].message.content);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  getChat("Quem é o presidente do brasil?");
+  
 
 
  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import GenerateDiet from "../components/GenerateDiet";
-
+import axios from "axios";
 
 
 const Home = () => {
+
 
 
   return (
