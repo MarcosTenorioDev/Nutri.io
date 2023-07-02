0a6eb847f6cb2be20ee1ebfe112d34a14524a2f9
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2023-07-01 22:43:53 -0300

    :iphone: responsive: add responsive design to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,20 +7,24 @@ const Navbar = () => {
     return (
         <div className='navbarBackground'>
             <nav className="navbar">
-            <Link to = {`/`} className='logo'> 
-                NUTRI.IO
-            </Link>
-           
-            
+                <Link to={`/`} className='logo'>
+                    NUTRI.IO
+                </Link>
 
-             <ul className='navbar-list'>
-                <Link className='navButton'><img src={user} alt="" className='navBarIcon' /> Sign-In </Link>
-                <Link className='navButton'><img src={login} alt="" className='navBarIcon' />Login</Link>
-            </ul> 
-        </nav>
-        </div>  
+
+
+                <ul className='navbar-list'>
+                    <Link className='navButton'><img src={user} alt="" className='navBarIcon' /> Sign-In </Link>
+                    <Link className='navButton'><img src={login} alt="" className='navBarIcon' />Login</Link>
+                </ul>
+
+                <div id="loginMobile">
+                    <Link><img src={user} alt="" className='navBarIcon'/></Link>
+                </div>
+
+            </nav>
+        </div>
     );
-  };
-  
+};
+
 export default Navbar;
-  
